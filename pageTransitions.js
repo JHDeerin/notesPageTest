7d04820b75f074163865ef6de9010d819a7262ee
jDeerin-hackGT <2016deerinjo@gmail.com>
2018-06-13 09:45:17 -0400

    Initial Commit

---
+++ pageTransitions.js
@@ -0,0 +1,73 @@
+/**
+ * Based on Luigi De Rosa's tutorial for Smashing Magazine
+ * (original link: https://www.smashingmagazine.com/2016/07/improving-user-flow-through-page-transitions/)
+ */
+
+var cache = {};
+function loadPage(url) {
+  if (cache[url]) {
+    return new Promise(function(resolve) {
+      resolve(cache[url]);
+    });
+  }
+
+  return fetch(url, {
+    method: 'GET'
+  }).then(function(response) {
+    cache[url] = response.text();
+    return cache[url];
+  });
+}
+
+var mainNotes = document.querySelector('pre');
+
+function changePage() {
+  var url = window.location.href;
+
+  loadPage(url).then(function(responseText) {
+    var wrapper = document.createElement('div');
+        wrapper.innerHTML = responseText;
+
+    //var oldContent = document.querySelector('pre');
+    var newContent = wrapper.querySelector('pre');
+
+    mainNotes.innerHTML = newContent.innerHTML;
+    //mainNotes.appendChild(newContent);
+    //animate(oldContent, newContent);
+  });
+}
+
+/*
+function animate(oldContent, newContent) {
+  oldContent.style.position = 'absolute';
+
+  var fadeOut = oldContent.animate({
+    opacity: [1, 0]
+  }, 1000);
+
+  var fadeIn = newContent.animate({
+    opacity: [0, 1]
+  }, 1000);
+
+  fadeIn.onfinish = function() {
+    oldContent.parentNode.removeChild(oldContent);
+  };
+}*/
+
+window.addEventListener('popstate', changePage);
+
+document.addEventListener('click', function(e) {
+  var el = e.target;
+
+  while (el && !el.href) {
+    el = el.parentNode;
+  }
+
+  if (el) {
+    e.preventDefault();
+    history.pushState({}, "", el.href);
+    changePage(el.href);
+
+    return;
+  }
+});
