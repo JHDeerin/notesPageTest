3ee90a0088ecb64fddb313ee4ee1a9c7120cac33
JHDeerin <2016deerinjo@gmail.com>
2019-10-11 13:33:16 -0400

    Added Google-style indenting for lists converted from .txt to Markdown

--- txtNotesToMd.py
+++ txtNotesToMd.py
@@ -7,6 +7,68 @@ import sys
 
 from htmlFromNotesDirectory import getTitleAndDateFromTitleLine
 
+def isPossibleCodeLine(line):
+    '''
+    Returns true if the given line should go in a Markdown code block because it
+    it's a formula/line of code/ASCII diagram/etc.
+    '''
+    strippedLine = line.lstrip()
+    return len(strippedLine) > 0 and not strippedLine.startswith('-')
+
+def softWrapTextLine(line, indentation, maxLineLength=80):
+    '''
+    "Soft wraps" a line to the given maximum length (such that the wrapped part
+    matches the given indentation level)
+    '''
+    outputLine = ''
+    textRemaining = True
+    isFirstLine = True
+    while textRemaining:
+        if len(line) <= maxLineLength:
+            outputLine += line
+            textRemaining = False
+        else:
+            indexToSplitOn = 0
+            for word in line.split(' '):
+                newLineLength = indexToSplitOn + len(word) + 1
+                if isFirstLine:
+                    newLineLength += indentation
+                if newLineLength <= maxLineLength:
+                    indexToSplitOn += len(word) + 1 # +1 to include spaces
+                else:
+                    if len(word) + 1 > maxLineLength - indentation:
+                        # special case for if the word itself is too long
+                        # to fit on one line
+                        indexToSplitOn = maxLineLength
+                    break
+
+            isFirstLine = False
+            outputLine += line[0 : indexToSplitOn] + '\n'
+            # add padding to the beginning of the new line
+            line = (' ' * indentation) + (line[indexToSplitOn:].lstrip())
+    return outputLine
+
+def convertLineToGoogleMarkdownStyle(line):
+    '''
+    Format the given line into Google's Markdown style for lists
+    '''
+    if isPossibleCodeLine(line) or len(line.strip('-')) == 0:
+        # Don't try to format it if it isn't a list-style line
+        return line
+    if len(line.strip()) == 0:
+        return ''
+
+    strippedLine = line.lstrip()
+
+    indentation = len(line) - len(strippedLine)
+    roundedIndentation = math.ceil(indentation / 4) * 4
+
+    # Indent 1 level more than beginning bullet
+    indentedLine = softWrapTextLine(strippedLine[1:].strip(), roundedIndentation + 4)
+    spacedLine = f'-   {indentedLine}'
+    tabbedLine = (' ' * roundedIndentation) + spacedLine
+    return tabbedLine
+
 def convertNoteTextToMarkdown(noteText):
     '''
     Takes the given .txt note text as a string and returns a string containing
@@ -23,6 +85,7 @@ def convertNoteTextToMarkdown(noteText):
 
     outputMarkdown += f'# {titleString}\n\n## {dateString}\n\n'
     isCodeBlock = False
+    codeBlockIndentation = 0
     for line in noteTextLines:
 
         # Handle code block/formulas case
@@ -30,12 +93,15 @@ def convertNoteTextToMarkdown(noteText):
         indentation = len(line) - len(strippedLine)
 
         wasCodeBlock = isCodeBlock
-        isCodeBlock = len(strippedLine) > 0 and not strippedLine.startswith('-')
+        isCodeBlock = isPossibleCodeLine(line)
 
         if (not wasCodeBlock and isCodeBlock) or (wasCodeBlock and not isCodeBlock):
-            outputMarkdown += "```\n"
+            if isCodeBlock:
+                codeBlockIndentation = indentation
+            outputMarkdown += f"{' '*codeBlockIndentation}```\n"
 
-        outputMarkdown += f'{line}\n'
+        tabbedLine = convertLineToGoogleMarkdownStyle(line)
+        outputMarkdown += f'{tabbedLine}\n'
 
     return outputMarkdown
 
