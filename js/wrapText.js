4193356463586548ca0cab0e4199c7c3626d9fb4
jDeerin-hackGT <2016deerinjo@gmail.com>
2019-12-05 15:09:24 -0500

    Minor refactoring for wrapText.js

--- js/wrapText.js
+++ js/wrapText.js
@@ -13,36 +13,42 @@ function softWrapTextLines(rawText, maxLineLength) {
         return str.replace(/^\s*/g, '');
     }
 
-    originalText.forEach(function(line) {
+    function softWrapString(line) {
         let textRemaining = true;
-        let indentation = -1;    // -1 means indentation hasn't been set yet
-        while (textRemaining) {
-            if (line.length <= maxLineLength) {
-                brokenLines += line + '\n';
-                textRemaining = false;
-            } else {
-                if (indentation == -1) {
-                    indentation = line.length - lstrip(line).length;
-                }
+        let indentation = line.length - lstrip(line).length;
 
-                let indexToSplitOn = 0;
-                line.split(' ').some(function(word) {
-                    if (indexToSplitOn + word.length + 1 <= maxLineLength) {
-                        indexToSplitOn += word.length + 1 // +1 to include spaces
-                    } else {
-                        if (word.length + 1 > maxLineLength - indentation) {
-                            // special case for if the word itself is too long
-                            // to fit on one line
-                            indexToSplitOn = maxLineLength
-                        }
-                        return true; // Acts as a 'break' statement
-                    }
-                });
-                brokenLines += line.slice(0, indexToSplitOn) + '\n';
-                line = ' '.repeat(indentation) + lstrip(line.slice(indexToSplitOn));
+        line = lstrip(line);
+
+        while (textRemaining) {
+            if (line.length <= maxLineLength - indentation) {
+                brokenLines += ' '.repeat(indentation) + line + '\n';
+                textRemaining = false;
+                break;
             }
+
+            // determine which index to break the line on by iterating through
+            // each word and adding its length
+            let indexToSplitOn = 0;
+            line.split(' ').some(function(word) {
+                if (indexToSplitOn + word.length + 1 <= maxLineLength - indentation) {
+                    indexToSplitOn += word.length + 1 // +1 to include spaces
+                } else {
+                    // special case for if the word itself is too long to fit on
+                    // one line
+                    if (word.length + 1 > maxLineLength - indentation) {
+                        indexToSplitOn = maxLineLength
+                    }
+                    return true; // Acts as a 'break' statement
+                }
+            });
+
+            // break line, saving the broken-off part
+            brokenLines += ' '.repeat(indentation) + line.slice(0, indexToSplitOn) + '\n';
+            line =  lstrip(line.slice(indexToSplitOn));
         }
-    });
+    }
+
+    originalText.forEach(softWrapString);
 
     return brokenLines + '\n' // add extra line of padding at the bottom
 }
